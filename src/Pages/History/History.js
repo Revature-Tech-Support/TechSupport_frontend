7741f32b270fc99f019654504d44373a46cc7077
LanChi Pham <Lpham2525@gmail.com>
2021-09-16 18:17:23 -0400

    removed card from history page

--- src/Pages/History/History.js
+++ src/Pages/History/History.js
@@ -15,19 +15,13 @@ const History = () => {
 
       <div className='container'>
         {/*<h2>Client: {data.username}</h2>
-        <h3>Issue #: {data.ticketId}</h3>
-          <h3>Subject: {data.subject}</h3> 
-          <h3>Transcript: {data.transcripts}</h3>*/}
-      <div className='card text-white bg-primary mb-3' style={{ maxWidth: '18rem'}}>
-        <div className='card-header'>Issue # 4</div>
-        <div className='card-body'>
-          <h5 className='card-title'>Client: Cockroach Team</h5>
-          <h5 class='card-text'>Subject: Won't die</h5>
-          
-        </div>
-      </div>
+        <h3>Issue # {data.ticketId}</h3>
+          <h3>Subject: {data.subject}</h3>*/}
       <div className="sheet">
-        <h2>Transcript:</h2>
+        <div className='card-header'>Issue # 4</div>
+          <h5 className='card-title'>Client: Cockroach Team</h5>
+          <h5 className='card-text'>Subject: Won't die</h5>
+        <h5>Transcript:</h5>
         <p><b>Agent:</b> Hello, we read about your issue and its details. Is there anything you would like to add?</p>
         <p><b>Cockroach Team:</b> Yeah! We're getting shuffled around more than a deck of cards at a blackjack table!</p>
         <p><b>Agent:</b> ...</p>
