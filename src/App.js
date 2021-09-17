990d7e4429d5167030200364482fc96d50b0765c
tam <87770920+tamhpn@users.noreply.github.com>
2021-09-17 15:31:07 -0500

    Passing props through Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Route, Redirect, Switch } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import LoginOrRegister from './Pages/LoginOrRegister';
 import Ticket from './Pages/Ticket';
 import Chat from './Pages/Chat';
@@ -9,23 +9,22 @@ import Queue from './Pages/Queue';
 import History from './Pages/History';
 
 const App = () => {
+  const [user, setUser] = useState(window.localStorage.getItem('username'));
 
-const [user] = useState(null);
-  //return user == null ? (
-  return(
+  return (
     <>
       <Router>
         <Switch>
-          <Redirect exact from="/" to="/login" />
-          <Route path="/login" component= {LoginOrRegister} />
-          <Route path="/createTicket" component={Ticket} />
-          <Route path="/chat" component={Chat} />
-          <Route path="/queue" component={Queue} />
-          <Route path="/history" component={History} />
+          <Redirect exact from='/' to='/login' />
+          <Route path='/login' component={LoginOrRegister} />
+          <Route path='/chat' component={() => <Chat user={user} />} />
+          <Route path='/createTicket' component={Ticket} />
+          <Route path='/queue' component={Queue} />
+          <Route path='/history' component={History} />
         </Switch>
       </Router>
     </>
-  ); 
-}
+  );
+};
 
 export default App;
