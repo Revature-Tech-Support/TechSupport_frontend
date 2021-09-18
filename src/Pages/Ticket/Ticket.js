158f8fb80de0898b120554c6349a862b38f3d588
tam <87770920+tamhpn@users.noreply.github.com>
2021-09-18 15:35:11 -0500

    update states for input boxes

--- src/Pages/Ticket/Ticket.js
+++ src/Pages/Ticket/Ticket.js
@@ -5,38 +5,44 @@ import Footer from '../../Components/Footer';
 import axios from 'axios';
 
 const Ticket = () => {
+  // states
+  const [subject, setSubject] = useState('');
+  const [issue, setIssue] = useState('');
+  const [ticket, setTicket] = useState([]);
 
-// states
-const [input, setInput] = useState('');
-const [ticket, setTicket] = useState([]);
+  const fileUpload = useRef();
 
-const subject = useRef();
-const issueDetails = useRef();
-const fileUpload = useRef();
-
-const handleInputChange = event => {
-  setInput(event.target.value);
-};
-
-const submitTicket = event => {
-  event.preventDefault();
-  if (issueDetails !== '') {
-    axios.post('/tickets', { 
-      subject: subject, 
-      // issue: issueDetails, 
-      files: fileUpload })
-  .then(({ data }) => {
-    if (data) {
-      console.log(data)
-      alert(`Your ticket with ID: ${ticket.id} has been submitted!`) 
-      setInput('');
-    } else {
-      alert('Please enter in details about your issue.')
-    }
-  })
-  .catch(err => console.error(err))
+  const handleSubjectChange = event => {
+    setSubject(event.target.value);
   };
-};
+
+  const handleIssueChange = event => {
+    setIssue(event.target.value);
+  };
+
+  const submitTicket = event => {
+    event.preventDefault();
+    console.log(subject);
+    console.log(issue);
+    if (issue !== '') {
+      axios.post('/tickets', {
+        subject,
+        issue
+        // files: fileUpload
+      })
+        .then(({ data }) => {
+          if (data) {
+            console.log(data);
+            window.alert(`Your ticket with ID: ${ticket.id} has been submitted!`);
+            setSubject('');
+            setIssue('');
+          } else {
+            window.alert('Please enter in details about your issue.');
+          }
+        })
+        .catch(err => console.error(err));
+    }
+  };
 
   return (
     <>
@@ -48,40 +54,44 @@ const submitTicket = event => {
           <div className='row justify-content-center'>
             <label htmlFor='subject' className='form-label'>Subject</label>
             <div className='col-3 p-3'>
-              <input 
-                type='text' 
-                className='form-control' 
-                ref={subject} 
-                onChange={event => handleInputChange(event)}
-                />
+              <input
+                type='text'
+                className='form-control'
+                input={subject}
+                onChange={event => handleSubjectChange(event)}
+              />
             </div>
           </div>
           <div className='mb-3'>
             <label htmlFor='textArea' className='form-label'>Issue</label>
-            <textarea 
-              className='form-control' 
-              ref={issueDetails} id='textArea' 
-              rows='4' 
-              placeholder='Please enter in details about your issue.' 
-              onChange={event => handleInputChange(event)}
+            <textarea
+              className='form-control'
+              id='textArea'
+              rows='4'
+              placeholder='Please enter in details about your issue.'
+              input={issue}
+              onChange={event => handleIssueChange(event)}
               required
             >
+              {}
             </textarea>
           </div>
           <div className='mb-3'>
             <label htmlFor='formFileMultiple' className='form-label'>Upload file(s) here</label>
-            <input 
-              className='form-control' 
-              ref={fileUpload} 
-              type='file' 
-              onChange={event => handleInputChange(event)} 
-              multiple />
+            <input
+              className='form-control'
+              ref={fileUpload}
+              type='file'
+              // onChange={event => handleInputChange(event)}
+              multiple
+            />
           </div>
-          <button 
-            type='submit' 
+          <button
+            type='submit'
             className='btn btn-primary'
             onClick={submitTicket}
-          >Submit
+          >
+            Submit
           </button>
         </form>
       </div>
