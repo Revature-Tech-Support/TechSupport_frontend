715860400ba7c79097c49c6eb481cc41a4e3522f
LanChi Pham <Lpham2525@gmail.com>
2021-09-18 08:38:25 -0400

    modified Navbar to include link to Revature home page

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,14 +5,14 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Revature</a>
-            <div className="navbar-nav">
-              <a className="nav-link" href="/login">Login/Register</a>
-              <a className="nav-link" href="/createTicket">Create Ticket</a>
-              <a className="nav-link" href="/chat">Chat</a>
-              <a className="nav-link" href="/queue">Queue</a>
-              <a className="nav-link" href="/history">History</a>
-            </div>
+          <a className="navbar-brand" href="www.revature.com">Revature</a>
+          <div className="navbar-nav">
+            <a className="nav-link" href="/login">Login/Register</a>
+            <a className="nav-link" href="/createTicket">Create Ticket</a>
+            <a className="nav-link" href="/chat">Chat</a>
+            <a className="nav-link" href="/queue">Queue</a>
+            <a className="nav-link" href="/history">History</a>
+          </div>
         </div>
       </nav>
     </>
