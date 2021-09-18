de05e93cdc77d3447fb52019d3e0bf59e17b7fad
LanChi Pham <Lpham2525@gmail.com>
2021-09-18 18:32:29 -0400

    second commit

--- src/Pages/Ticket/Ticket.js
+++ src/Pages/Ticket/Ticket.js
@@ -73,7 +73,11 @@ const Ticket = () => {
               onChange={event => handleIssueChange(event)}
               required
             >
+<<<<<<< HEAD
               {}
+=======
+              { }
+>>>>>>> 928d94c... incorporated Tam's changes
             </textarea>
           </div>
           <div className='mb-3'>
