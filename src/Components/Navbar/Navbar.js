7097c2011f4f5307932b79a954d35d359d0ad80e
LanChi Pham <Lpham2525@gmail.com>
2021-09-19 11:24:33 -0400

    experimented with navbar options based on user login/signout

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,36 +2,57 @@ import React, { useState } from 'react';
 
 const Navbar = () => {
   const [user] = useState(['client'])
-  
-  return (
-    user == 'agent' ? (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="www.revature.com">Revature</a>
-          <div className="navbar-nav">
-            <a className="nav-link" href="/login">Login/Register</a>
-            <a className="nav-link" href="/chat">Chat</a>
-            <a className="nav-link" href="/queue">Queue</a>
-            <a className="nav-link" href="/history">History</a>
+
+  const signOut = () => {
+    sessionStorage.removeItem('user');
+    sessionStorage.clear();
+    window.location = './login';
+  }
+
+  if (user == null) {
+    return (
+      <>
+        <nav className='navbar navbar-expand-lg navbar-light bg-secondary'>
+          <div className='container-fluid'>
+            <a className='navbar-brand' href='www.revature.com'>Revature</a>
+            <div className='navbar-nav'>
+              <a className='nav-link' href='/login'>Login/Register</a>
+            </div>
           </div>
-        </div>
-      </nav>
-    </>
-    ) : (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="www.revature.com">Revature</a>
-          <div className="navbar-nav">
-            <a className="nav-link" href="/login">Login/Register</a>
-            <a className="nav-link" href="/createTicket">Create Ticket</a>
-          </div>
-        </div>
-      </nav>
-    </>
+        </nav>
+      </>
     )
-  );
-}
+  } else if (user == 'agent') {
+    return (
+      <>
+        <nav className='navbar navbar-expand-lg navbar-light bg-secondary'>
+          <div className='container-fluid'>
+            <a className='navbar-brand' href='www.revature.com'>Revature</a>
+            <div className='navbar-nav'>
+              <a className='nav-link' href='/login' onClick={event => signOut(event)}>Sign Out</a>
+              <a className='nav-link' href='/chat'>Chat</a>
+              <a className='nav-link' href='/queue'>Queue</a>
+              <a className='nav-link' href='/history'>History</a>
+            </div>
+          </div>
+        </nav>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <nav className='navbar navbar-expand-lg navbar-light bg-secondary'>
+          <div className='container-fluid'>
+            <a className='navbar-brand' href='www.revature.com'>Revature</a>
+            <div className='navbar-nav'>
+              <a className='nav-link' href='/login' onClick={event => signOut(event)}>Sign Out</a>
+              <a className='nav-link' href='/createTicket'>Create Ticket</a>
+            </div>
+          </div>
+        </nav >
+      </>
+    );
+  };
+};
 
 export default Navbar;
