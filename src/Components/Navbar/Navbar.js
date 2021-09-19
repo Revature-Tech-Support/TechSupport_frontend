9b0fc78258015f248326c2890616704fa4fd7bee
LanChi Pham <Lpham2525@gmail.com>
2021-09-18 20:48:21 -0400

    applied ternary in Navbar, per Jesus's suggestion

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+  const [user] = useState(['agent'])
+  
   return (
+    user === 'agent' ? (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
         <div className="container-fluid">
           <a className="navbar-brand" href="www.revature.com">Revature</a>
           <div className="navbar-nav">
             <a className="nav-link" href="/login">Login/Register</a>
-            <a className="nav-link" href="/createTicket">Create Ticket</a>
             <a className="nav-link" href="/chat">Chat</a>
             <a className="nav-link" href="/queue">Queue</a>
             <a className="nav-link" href="/history">History</a>
@@ -16,6 +18,19 @@ const Navbar = () => {
         </div>
       </nav>
     </>
+    ) : (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="www.revature.com">Revature</a>
+          <div className="navbar-nav">
+            <a className="nav-link" href="/login">Login/Register</a>
+            <a className="nav-link" href="/createTicket">Create Ticket</a>
+          </div>
+        </div>
+      </nav>
+    </>
+    )
   );
 }
 
