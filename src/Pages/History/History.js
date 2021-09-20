b0a4a98fb7420293418ee913e84efd80ec264b69
LanChi Pham <Lpham2525@gmail.com>
2021-09-20 15:47:51 -0400

    history page won't work

--- src/Pages/History/History.js
+++ src/Pages/History/History.js
@@ -1,39 +1,58 @@
-import React from 'react'; 
+import React from 'react';
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
 
 const History = () => {
+  // const displayTranscripts = () => {
+  //   const transcripts = JSON.parse(window.localStorage.getItem('transcripts'));
+  //   console.log(transcripts);
+  //   return transcripts ? (
+  //   transcripts.map(transcripts => {
+  //     return (
+  //       <div className='transcripts' key={transcripts.id}>
+  //         <h3>{transcripts.name}</h3>
+  //       </div>
+  //       );
+  //     })
+  //   ) : (
+  //     <h3>No transcripts yet</h3>
+  //   );
+  // };
 
-  const displayTranscripts = () => {
-    const transcripts = JSON.parse(localStorage.getItem('transcripts'))
-    console.log(transcripts)
-    return transcripts ? (
-      transcripts.map(transcripts => {
-        return (
-          <div className='transcripts' key={transcripts.id}>
-            <h3>{transcripts.name}</h3>
-          </div>
+  const displayData = () => {
+    const data = JSON.parse(window.localStorage.getItem('transcripts'));
+    console.log(data);
+    return data
+      ? (
+          data.map(data => {
+            return (
+              <div className='data' key={data.id}>
+                <h3>{data.name}</h3>
+              </div>
+            );
+          })
+        )
+      :
+        (
+          <h3>No data yet</h3>
         );
-      })
-    ) : (
-      <h3>No transcripts yet</h3>
-    );
   };
 
   return (
     <>
-    <Navbar />
+      <Navbar />
       <div className='jumbotron'>
         <h1 className='display-4'>Welcome to the transaction history page!</h1>
-        <p className='lead'>A good place to take a walk down memory lane...or to do some light reading.</p>
-        <p className='lead'></p>
+        <p className='lead'>A good place to take a walk down memory lane...or to do some light reading</p>
+        <p className='lead'>{}</p>
         <hr className='my-4' />
       </div>
 
       <div className='container'>
-        {displayTranscripts()}
-    </div>
-    <Footer />
+        {/* {displayTranscripts()} */}
+        {displayData()}
+      </div>
+      <Footer />
     </>
   );
 };
--- src/Pages/Queue/Queue.js
+++ src/Pages/Queue/Queue.js
@@ -1,36 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
 
 const Queue = ({ ticketId }) => {
-
-  //const [userId, setUserId] = useState(window.localStorage.getItem('userId'));
+  // const [userId, setUserId] = useState(window.localStorage.getItem('userId'));
 
   const startChat = event => {
-    console.log(event)
-      let openChat = window.confirm('This will open a chat with the corresponding client. Proceed?')
-      if (openChat) {
-        window.location = '/chat'
+    if (window.confirm('This will open a chat with the corresponding client. Proceed?')) {
+      window.location = '/chat';
     }
   };
 
   return (
     <>
-    <Navbar />
-    <div className='jumbotron'>
-      <h1 className='display-4'>Welcome to back to work!</h1>
-      <p className='lead'><b>Remember the golden question:</b> Have you tried turning it off and on again?</p>
-      <p className='lead'></p>
-      <hr className='my-4' />
-    </div>
-    <div className='container'>
-      <button 
-        className='btn btn-success'
-        onClick={event => startChat(event)}
-      >Start chat with next client
-      </button>  
-    </div>
-    <Footer />
+      <Navbar />
+      <div className='jumbotron'>
+        <h1 className='display-4'>Welcome to back to work!</h1>
+        <p className='lead'><b>Remember the golden question:</b> Have you tried turning it off and on again?</p>
+        <p className='lead'>{}</p>
+        <hr className='my-4' />
+      </div>
+      <div className='container'>
+        <button
+          className='btn btn-success'
+          onClick={event => startChat(event)}
+        >Start chat with next client
+        </button>
+      </div>
+      <Footer />
     </>
   );
 };
