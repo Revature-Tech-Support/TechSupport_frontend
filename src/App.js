617bd98c70d77781c50847b1951606e829060900
LanChi Pham <Lpham2525@gmail.com>
2021-09-20 17:16:57 -0400

    resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import History from './Pages/History';
 // import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 
 const App = () => {
-  const [user, setUser] = useState(window.localStorage.getItem('username'));
+  const [user] = useState(window.localStorage.getItem('username'));
 
   return (
     <>
