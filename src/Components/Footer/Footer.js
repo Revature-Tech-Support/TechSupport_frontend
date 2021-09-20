a350510acbf1fe9e9f472cfbf5f1605313098772
LanChi Pham <Lpham2525@gmail.com>
2021-09-19 21:28:25 -0400

    modified footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <nav className="navbar fixed-bottom bg-secondary">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 mx-auto">Revature &#169;2021</span>
+        <span className="navbar-brand mb-0 h1 mx-auto">Revature &#169;2021 All rights reserved.</span>
       </div>
     </nav>
   );
