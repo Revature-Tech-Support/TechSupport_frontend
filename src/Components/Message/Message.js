38c2b4257c1a0079b5e2273503c5bbf5b5fb05c3
tam <87770920+tamhpn@users.noreply.github.com>
2021-09-21 17:51:44 -0500

    fixed css after changing how user is stored

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -1,7 +1,8 @@
 const Message = ({ message }) => {
+  const user = JSON.parse(window.localStorage.getItem('user'));
   return (
     <div className='col'>
-      <li className={message.username === window.localStorage.getItem('username') ? 'sender' : 'receiver'}>
+      <li className={message.username === user.username ? 'sender' : 'receiver'}>
         <div className='msg'>
           {/* <p>{data.sender.username}</p> */}
           {/* <div className='msgText'> {data.data.text}</div> */}
