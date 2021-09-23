44e269ef3f7e44609e7562fba5cf763611726d99
tam <87770920+tamhpn@users.noreply.github.com>
2021-09-22 23:50:04 -0500

    single quotes

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -1,18 +1,18 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from 'react';
 // import Message from '../../Components/Message';
-import MessageCopy from "../../Components/Message";
-import "./Chat.css";
-import axios from "axios";
-import Layout from "../../Components/Layout";
-import { ReactComponent as RightArrow } from "../../resources/svg/circle-right.svg";
-import { ReactComponent as TimesCircle } from "../../resources/svg/times-circle.svg";
+import MessageCopy from '../../Components/Message';
+import './Chat.css';
+import axios from 'axios';
+import Layout from '../../Components/Layout';
+import { ReactComponent as RightArrow } from '../../resources/svg/circle-right.svg';
+import { ReactComponent as TimesCircle } from '../../resources/svg/times-circle.svg';
 
 const Chat = () => {
-  const user = JSON.parse(window.localStorage.getItem("user"));
+  const user = JSON.parse(window.localStorage.getItem('user'));
   const webSocket = useRef();
 
   // states
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const [messages, setMessages] = useState([]);
 
   // event handlers
@@ -22,15 +22,15 @@ const Chat = () => {
 
   const sendMessage = (event) => {
     event.preventDefault();
-    if (input !== "") {
+    if (input !== '') {
       webSocket.current.send(
         JSON.stringify({
           username: user.username,
           message: input,
-          timestamp: Date.now(),
+          timestamp: Date.now()
         })
       );
-      setInput("");
+      setInput('');
     }
   };
 
@@ -47,7 +47,7 @@ const Chat = () => {
       )) {
         // clears client from localStorage, thereby logging them out
         window.localStorage.clear();
-        window.location = "/";
+        window.location = '/';
       }
     } else {
       // this is for tech support and redirects them to the queue
@@ -63,30 +63,30 @@ const Chat = () => {
     // 'axios.get' line needs to be replaced once transcripts is connected to the frontend
     // for now, using jsonplaceholder dummy data to demonstrate functionality
     axios
-      .get("https://jsonplaceholder.typicode.com/comments")
+      .get('https://jsonplaceholder.typicode.com/comments')
       // axios.get(`/transcripts/${userId}`, {
       //   transcripts: transcripts
       // })
       .then(({ data }) => {
         console.log(data);
-        window.localStorage.setItem("transcripts", JSON.stringify(data));
-        window.open("/history", "_blank");
+        window.localStorage.setItem('transcripts', JSON.stringify(data));
+        window.open('/history', '_blank');
       })
       .catch((err) => console.error(err));
   };
 
   // console logging status updates based on whether WebSocket is connected
   useEffect(() => {
-    webSocket.current = new window.WebSocket("ws://localhost:8080/ws");
+    webSocket.current = new window.WebSocket('ws://localhost:8080/ws');
 
     webSocket.current.onopen = () => {
-      console.log("Connected to websocket");
+      console.log('Connected to websocket');
     };
     webSocket.current.onerror = (error) => {
       console.log(error);
     };
     webSocket.current.onclose = () => {
-      console.log("Disconnected from websocket");
+      console.log('Disconnected from websocket');
     };
   }, []);
 
@@ -99,30 +99,30 @@ const Chat = () => {
 
   return (
     <Layout
-      imgSrc=""
-      imgAlt=""
-      imgWidth=""
-      imgHeight=""
-      title=""
-      desc=""
-      jtClass="pt-1"
-      childClass="chat-page-cc"
+      imgSrc=''
+      imgAlt=''
+      imgWidth=''
+      imgHeight=''
+      title=''
+      desc=''
+      jtClass='pt-1'
+      childClass='chat-page-cc'
     >
-      <div className="chat-app-cc">
-        <div className="chat-header-cc">
+      <div className='chat-app-cc'>
+        <div className='chat-header-cc'>
           <h3>Chatting with Mr. Tyson</h3>
           <TimesCircle onClick={(event) => handleCloseChat(event)} />
         </div>
-        <div className="messages-cc">
+        <div className='messages-cc'>
           {messages.map((message, idx) => (
             <MessageCopy key={idx} message={message} />
           ))}
         </div>
-        <div className="chat-input-div">
+        <div className='chat-input-div'>
           <input
-            type="text"
-            name="inputText"
-            placeholder="Type your message here"
+            type='text'
+            name='inputText'
+            placeholder='Type your message here'
             value={input}
             onChange={(event) => handleInputChange(event)}
             onKeyPress={(event) => handleEnterKeyPress(event)}
@@ -130,8 +130,8 @@ const Chat = () => {
           />
           <RightArrow onClick={(event) => sendMessage(event)} />
           <button
-            className="button btn btn-outline-primary transcripts-cc"
-            target="_blank"
+            className='button btn btn-outline-primary transcripts-cc'
+            target='_blank'
             onClick={(event) => getTranscripts(event)}
           >
             Transcripts
